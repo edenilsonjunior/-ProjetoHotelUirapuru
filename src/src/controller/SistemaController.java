5bbe817cc7a63b7c7804d460cb8539c725f49ec0
Edenilson Junior <edenilsonju10@gmail.com>
2023-12-18 01:17:18 -0300

    Implementacao da permanencia de dados (mockados)

--- src/src/controller/SistemaController.java
+++ src/src/controller/SistemaController.java
@@ -1,6 +1,15 @@
 package controller;
 
-import javax.swing.JOptionPane;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import model.alojamento.*;
 import model.hotel.*;
@@ -13,36 +22,61 @@ public class SistemaController {
     private Hotel hotel;
     private Pessoa logado;
 
-    // Construtor
     public SistemaController(Hotel hotel){
         this.hotel = hotel;
         logado = null;
 
-        hotel.addFuncionario(new Funcionario(01, "admin"));
+        verificarPrimeiroAcesso();
     }
 
     // Inicia o sistema
-    public void iniciarSistema(){
+    public void iniciarSistema() {
+
+        boolean sair = false;
+        do {
+            try {
+                String[] usuario = SistemaView.menuLogin();
+
+                if (usuario == null || usuario.length == 0) {
+                    // Se usuário fechou a tela de login, sair do loop
+                    break;
+                }
+
+                logado = descobrirLogado(hotel, usuario);
+
+                if((logado instanceof Funcionario) && logado.getLogin().equals("admin")){
+                    funcoesAdmin();
+                }
+                else if(logado instanceof Funcionario) {
+                    funcoesFuncionario();
+                }
+                else if(logado instanceof Hospede) {
+                    funcoesHospede();
+                }
+                else {
+                    SistemaView.erroLogin();
+                }
+
+                sair = SistemaView.verificarSair();
+            } catch (Exception e) {
+                // Tratar a exceção aqui
+                e.printStackTrace();
+                System.out.println("Ocorreu um erro: " + e.getMessage());
+                break;
+            }
+        } while (!sair);
         
-        String[] usuario = SistemaView.menuLogin();
-
-        logado = descobrirLogado(hotel, usuario);
-
-        if((logado instanceof Funcionario) && logado.getLogin().equals("admin")){
-            funcoesAdmin();
-        }
-        else if(logado instanceof Funcionario) {
-            funcoesFuncionario();
-        }
-        else if(logado instanceof Hospede) {
-            funcoesHospede();
-        }
-        else {
-            JOptionPane.showMessageDialog(null, "Usuário ou senha incorretos!");
-        }
+        salvarEstadoHotel(hotel);
+        SistemaView.mensagemFinal();
     }
 
 
+
+
+
+
+
+        
     private Pessoa descobrirLogado(Hotel hotel, String[] usuario) {
             for (Funcionario funcionario : hotel.getFuncionarios()) {
                 if (usuario[0].equals(funcionario.getLogin()) && usuario[1].equals(funcionario.getSenha())) {
@@ -85,16 +119,16 @@ public class SistemaController {
             escolha = SistemaView.menuFuncionario();
 
             switch (escolha) {
-//                case CADASTRAR_HOSPEDAGEM:
-//                    hotel.addHospedagem(SistemaView.cadastrarHospedagem(hotel.getAcomodacoes()));
-//                    break;
+               case CADASTRAR_HOSPEDAGEM:
+                   hotel.addHospedagem(SistemaView.cadastrarHospedagem(hotel.getAcomodacoes()));
+                   break;
               case REMOVER_HOSPEDAGEM:
                     Hospedagem dadosDaHospedagem = SistemaView.removerHospedagem(hotel);
                     if (dadosDaHospedagem != null) {
                         SistemaView.relatorioSaidaHospede(dadosDaHospedagem);
                         hotel.removeReserva(dadosDaHospedagem);
                     }
-//                break;
+               break;
                 case LISTAR_ACOMODACOES:
                     SistemaView.relatorioAcomodacoes(hotel.getHospedagens(), hotel.getAcomodacoes());
                     break;
@@ -126,6 +160,48 @@ public class SistemaController {
 //
 //        } while (escolha != OpcoesHospede.SAIR);
     }
+
+    // Verifica se é o primeiro acesso ao sistema
+    private void verificarPrimeiroAcesso(){
+
+        String caminhoData = "data/hotel.json";
+        
+        if (Files.exists(Paths.get(caminhoData))) 
+        {
+           // Criar o objeto Gson
+            Gson gson = new Gson();
+            
+            // Ler o arquivo JSON
+            try (Reader reader = new FileReader("data/hotel.json")) {
+    
+                // Definir o tipo do objeto a ser lido
+                Type hotelType = new TypeToken<Hotel>(){}.getType();
+    
+                // Converter JSON para Java Object
+                Hotel hotel = gson.fromJson(reader, hotelType);
+    
+                // Inicializar o sistema com os dados do hotel
+                this.hotel = hotel;
+    
+            } catch (IOException e) {
+                e.printStackTrace();
+            } 
+        }
+        else{
+            this.hotel.addFuncionario(new Funcionario(01, "admin"));
+        }
+    }
+
+    private void salvarEstadoHotel(Hotel hotel) {
+        Gson gson = new Gson();
+
+        try (FileWriter writer = new FileWriter("data/hotel.json")) {
+            gson.toJson(hotel, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
 
