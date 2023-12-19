9ccebfa39daebe3eb97f17b8b7b0ffdac6a22a7d
Edenilson Junior <edenilsonju10@gmail.com>
2023-12-19 05:05:06 -0300

    Implementacao do metodo menuGenerico() que serve para todos os enuns

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -3,11 +3,6 @@ package view;
 import java.util.List;
 
 import javax.swing.*;
-import controller.opcoes.OpcoesAdmin;
-import controller.opcoes.OpcoesFuncionario;
-import controller.opcoes.OpcoesHospedagem;
-import controller.opcoes.OpcoesHospede;
-import controller.opcoes.OpcoesRelatorios;
 import model.alojamento.Hospedagem;
 
 /**
@@ -24,103 +19,72 @@ public class Menu {
     public static String[] menuLogin() {
 
         JTextField campoLogin = new JTextField(10);
-        JTextField campoSenha = new JTextField(10);
-        
+        JPasswordField campoSenha = new JPasswordField(10);
 
         JPanel painel = new JPanel();
         painel.setLayout(new BoxLayout(painel, BoxLayout.Y_AXIS));
 
-        
-        painel.add(new JLabel("Login:"));
-        painel.add(campoLogin);
-        
-        painel.add(new JLabel("Senha:"));
-        painel.add(campoSenha);
-        
+        addComponente(painel, "Login:", campoLogin);
+        addComponente(painel, "Senha:", campoSenha);
 
-        int result = JOptionPane.showConfirmDialog(null, painel, "Preencha todos os campos", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-        if (result == JOptionPane.OK_OPTION) 
-        {
-            return new String[]{ campoLogin.getText(), campoSenha.getText() };
+        int result = criarTela(painel, "Login");
+
+        if (result == JOptionPane.OK_OPTION) {
+            String login = campoLogin.getText();
+            String senha = new String(campoSenha.getPassword());
+
+            if (login.isEmpty() || senha.isEmpty()) 
+            {
+                mensagemErro("Os campos de login e senha não podem estar vazios. Tente novamente.", "Erro");
+                return menuLogin(); // Chamada recursiva para exibir a tela novamente 
+            } 
+            else {
+                return new String[] {login, senha};
+            }
+        } else {
+            return null;
         }
-        return null;
     }
+
     
     /**
-     * Apresenta um menu de opções para interação com funcionalidades administrativas.
-     *
-     * @return A opção escolhida pelo usuário do tipo {@link OpcoesAdmin}.
-     *         Pode ser {@link OpcoesAdmin#CADASTRAR_FUNC}, {@link OpcoesAdmin#LISTAR_FUNC},
-     *         {@link OpcoesAdmin#REMOVER_FUNC}, ou {@link OpcoesAdmin#SAIR}.
+     * Cria um menu genérico com opções baseadas em uma enumeração.
+     * T extends Enum<T> significa que o tipo T deve ser um enum.
+     * Esse método tem o objetivo de lidar com todos os menus que possuem opções baseadas em enumerações.
+     * 
+     * @param <T>       o tipo de enumeração
+     * @param tipoEnum  a classe da enumeração
+     * @param titulo    o título do menu
+     * @return          a opção selecionada pelo usuário
      */
-    public static OpcoesAdmin menuAdmin() {
-
-        OpcoesAdmin[] opcoes = { OpcoesAdmin.CADASTRAR_FUNC, OpcoesAdmin.LISTAR_FUNC, OpcoesAdmin.REMOVER_FUNC, OpcoesAdmin.SAIR };
-
-        return (OpcoesAdmin) JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu Admin", JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
+    public static <T extends Enum<T>> T menuGenerico(Class<T> tipoEnum, String titulo) {
+        
+        return menuOpcoes(
+            tipoEnum, 
+            tipoEnum.getEnumConstants(),
+            titulo);
     }
 
 
     /**
-     * Apresenta um menu de opções para interação com funcionalidades de funcionário.
-     *
-     * @return A opção escolhida pelo usuário do tipo {@link OpcoesFuncionario}.
-     *         Pode ser {@link OpcoesFuncionario#OPCOES_HOSPEDAGEM}, {@link OpcoesFuncionario#RELATORIOS},
-     *         ou {@link OpcoesFuncionario#SAIR}.
+     *  Exibe uma lista de todas as hospedagens e retorna a hospedagem escolhida pelo usuário.
+     * 
+     * @param hospedagens Lista de hospedagens
+     * @return            A hospedagem escolhida pelo usuário
      */
-    public static OpcoesFuncionario menuFuncionario() {
+    public static Hospedagem escolherModificado(List<Hospedagem> hospedagens){
 
-        OpcoesFuncionario[] opcoes = { OpcoesFuncionario.OPCOES_HOSPEDAGEM, OpcoesFuncionario.RELATORIOS, OpcoesFuncionario.SAIR };
-
-        return (OpcoesFuncionario) JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu Funcionario", JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
-    }
-
-
-    /**
-     * Apresenta um menu de opções para interação com informações do hóspede.
-     *
-     * @return A opção escolhida pelo usuário do tipo {@link OpcoesHospede}.
-     *         Pode ser {@link OpcoesHospede#RELATORIO_CONSUMO}, {@link OpcoesHospede#RELATORIO_ESTADIA},
-     *         ou {@link OpcoesHospede#SAIR}.
-     */
-    public static OpcoesHospede menuHospede() {
-
-        OpcoesHospede[] opcoes = { OpcoesHospede.RELATORIO_CONSUMO, OpcoesHospede.RELATORIO_ESTADIA,OpcoesHospede.RELATORIO_PAGAMENTO_FATURADO, OpcoesHospede.SAIR };
-
-        return (OpcoesHospede) JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu Hospede", JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
-    }
-
-    public static OpcoesRelatorios menuRelatorioGerais(){
-
-        OpcoesRelatorios[] opcoes = { OpcoesRelatorios.RELATORIO_HOSPEDES, OpcoesRelatorios.RELATORIO_RESERVAS_HOJE, OpcoesRelatorios.RELATORIO_ACOMODACOES, OpcoesRelatorios.RELATORIO_FATURAMENTO, OpcoesRelatorios.RELATORIO_ATRASADOS, OpcoesRelatorios.SAIR };
-
-        return (OpcoesRelatorios) JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu Relatorios", JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
-    }
-
-    public static OpcoesHospedagem menuHospedagem(){
-            
-            OpcoesHospedagem[] opcoes = { OpcoesHospedagem.CADASTRAR_HOSPEDAGEM, OpcoesHospedagem.REMOVER_HOSPEDAGEM, OpcoesHospedagem.CADASTRAR_ACOMODACAO, OpcoesHospedagem.ADICIONAR_ACOMPANHANTE, OpcoesHospedagem.SAIR };
-    
-            return (OpcoesHospedagem) JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu Hospedagem", JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes[0]);
-    }
-
-    public static Hospedagem escolherModificado(List<Hospedagem> hospedagens)
-    {
-        // Instancia um novo StringBuilder para armazenar os códigos das hospedagens
-        StringBuilder codigos = new StringBuilder();
-
-
-        // Adiciona os códigos das hospedagens ao StringBuilder
+        String codigos = "";
         for (Hospedagem hospedagem : hospedagens) {
-            codigos.append(hospedagem.getCodigo()).append("\n");
+
+            int numero = hospedagem.getAcomodacao().getNumeroQuarto();
+            codigos += "Quarto " + numero + ": " + hospedagem.getCodigo() + "\n";
         }
 
-        // Exibe um diálogo para o usuário escolher o código da hospedagem
-        String codigoEscolhido = JOptionPane.showInputDialog(null, "Digite o código da hospedagem:\n" + codigos.toString(), "Escolher Hospedagem", JOptionPane.PLAIN_MESSAGE);
+        String escolhido = telaEscolherHospedagem(codigos);
         
-        // Retorna a hospedagem escolhida
         for (Hospedagem hospedagem : hospedagens) {
-            if (hospedagem.getCodigo() == Integer.parseInt(codigoEscolhido)) {
+            if (hospedagem.getCodigo() == Integer.parseInt(escolhido)) {
                 return hospedagem;
             }
         }
@@ -128,4 +92,93 @@ public class Menu {
     }
 
 
+    /**
+     * Exibe uma lista de todas as reservas e retorna a reserva escolhida pelo usuário.
+     * @param hospedagens Lista de reservas
+     * @return            A reserva escolhida pelo usuário    
+     */
+    public static Hospedagem escolherCheckIn(List<Hospedagem> hospedagens){
+
+        String codigos = "";
+        for (Hospedagem hospedagem : hospedagens) {
+            if (hospedagem.isStatus() == false) {
+
+                int numero = hospedagem.getAcomodacao().getNumeroQuarto();
+                codigos += "Quarto " + numero + ": " + hospedagem.getCodigo() + "\n";
+            }
+        }
+
+        String escolhido = telaEscolherHospedagem(codigos);
+
+        for (Hospedagem hospedagem : hospedagens) {
+            if (hospedagem.getCodigo() == Integer.parseInt(escolhido)) {
+                return hospedagem;
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * Adiciona um componente ao painel passado como parâmetro.
+     * @param painel O painel onde o componente será adicionado.
+     * @param titulo O título do componente.
+     * @param campo O componente a ser adicionado.
+     */
+    private static void addComponente(JPanel painel, String titulo, JComponent campo) {
+        painel.add(new JLabel(titulo));
+        painel.add(campo);
+    }
+
+
+    /**
+     * Cria uma tela com o painel passado como parâmetro.
+     * @param painel O painel a ser exibido na tela.
+     * @param titulo O título da tela.
+     * @return O resultado da tela.
+     */
+    private static int criarTela(JPanel painel, String titulo) {
+        return JOptionPane.showConfirmDialog(null, painel, titulo, JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+    }
+
+
+    /**
+     * Exibe uma mensagem de erro.
+     * @param mensagem A mensagem de erro.
+     * @param titulo O título da mensagem de erro.
+     */
+    private static void mensagemErro(String mensagem, String titulo) {
+        JOptionPane.showMessageDialog(null, mensagem, titulo, JOptionPane.ERROR_MESSAGE);
+    }
+
+
+    /**
+     * Exibe um diálogo de confirmação com a mensagem "Deseja realmente sair?".
+     * @param <T> O tipo do array de opções.
+     * @param tipo O tipo da classe do array de opções.
+     * @param opcoes O array de opções.
+     * @param titulo O título do diálogo.
+     * 
+     * @return A opção escolhida pelo usuário.
+     */
+    private static <T> T menuOpcoes(Class<T> tipo, T[] opcoes, String titulo) {
+        Object escolha = JOptionPane.showInputDialog(
+                                                    null, 
+                                                    "Escolha uma opção", 
+                                                    titulo, 
+                                                    JOptionPane.QUESTION_MESSAGE, 
+                                                    null, 
+                                                    opcoes, 
+                                                    opcoes[0]);
+        return tipo.cast(escolha);
+    }
+
+    private static String telaEscolherHospedagem(String codigos) {
+        return JOptionPane.showInputDialog(
+                                            null, 
+                                            "Digite o código da hospedagem:\n" + codigos, 
+                                            "Escolher Hospedagem", 
+                                            JOptionPane.PLAIN_MESSAGE);
+    }
 }
+    
