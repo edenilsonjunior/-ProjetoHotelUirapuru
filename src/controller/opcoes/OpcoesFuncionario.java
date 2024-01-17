e2ce7063dbfb304d313be642bd7221784a0d1553
Edenilson Junior <edenilsonju10@gmail.com>
2024-01-17 14:37:59 -0300

    Volta dos enuns de opcoes

---
+++ src/controller/opcoes/OpcoesFuncionario.java
@@ -0,0 +1,23 @@
+package controller.opcoes;
+
+/**
+ * Enumerador de opções para o funcionário.
+ * <p>
+ * Este enum representa as possíveis ações que um funcionário pode realizar.
+ */
+public enum OpcoesFuncionario {
+    /**
+     * Opção para gerenciar as hospedagens.
+     */
+    OPCOES_HOSPEDAGEM,
+
+    /**
+     * Opção para gerar relatórios.
+     */
+    RELATORIOS,
+
+    /**
+     * Opção para sair do sistema.
+     */
+    SAIR;
+}
